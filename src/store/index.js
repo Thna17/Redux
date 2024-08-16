6454bf322b9ee8d38e4a2c75892cb331f7ecc451
AnB School 0021 <anbschool0021@AnBs-21-Mac-mini.local>
2024-08-16 11:46:40 +0700

    Set up redux store using Redux Toolkit

---
+++ src/store/index.js
@@ -0,0 +1,33 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const songsSlice = createSlice({
+    name: 'song',
+    initialState: [],
+    reducers: {
+        addSong(state, action) {
+            state.push(action.payload);
+        },
+        removeSong(state, action) {
+            
+        }
+    }
+});
+
+const store = configureStore({
+    reducer: {
+        songs: songsSlice.reducer
+    }
+});
+
+// console.log(songsSlice.actions.addSong())
+const startingState = store.getState();
+console.log(JSON.stringify(startingState));
+
+store.dispatch(songsSlice.actions.addSong('Some Song'));
+// store.dispatch({
+//     type: 'song/removeSong',
+//     payload: 'New Song'
+// })
+
+const finalState = store.getState();
+console.log(JSON.stringify(finalState));
